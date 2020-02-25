4a17b82cb3072655fc7bee1666e260fc7cdb11cf
Hyginus Ukeh <ukeh.hyginus@techvibesltd.com>
2020-02-25 01:49:06 +0100

    Config : creating App Component

--- resources/js/components/App.js
+++ resources/js/components/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-function Example() {
+const App = () => {
     return (
         <div className="container">
             <div className="row justify-content-center">
@@ -17,8 +17,6 @@ function Example() {
     );
 }
 
-export default Example;
 
-if (document.getElementById('example')) {
-    ReactDOM.render(<Example />, document.getElementById('example'));
-}
+ReactDOM.render(<App />, document.getElementById('app'));
+
